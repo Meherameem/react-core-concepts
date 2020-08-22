a8f761ac2a9bf062b8c8101c16bbca7be935e87b
Meherameem <54639359+Meherameem@users.noreply.github.com>
2020-08-22 23:07:24 +0600

    apatoto sesh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+// eslint-disable-next-line
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const mainChar = ['basudev','arjun','droupadi','shakuni','yudhisthir']
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am a react person</p>
+        <Person name={mainChar[4]} job='king of Hastinapur'></Person>
+        <Person name={mainChar[3]} job='Start of all problems and destructions'></Person>
       </header>
     </div>
   );
+}
+function Person(props){ //props is a short name for property
+  const personStyle={
+    border:'2px solid yellow',
+    margin:'10px',
+    padding:'10px'
+    }
+  return <div style={personStyle}>
+    <h1>Name: {props.name}</h1>
+  <h3>Identity: {props.job}</h3>
+  </div>
 }
 
 export default App;
